b9cb01a6aa7f5f179c7046f6dca23402e0606534
metalhead6666 <noe.godinho.92@gmail.com>
2013-10-30 15:21:09 +0000

    Exportei BD v2

--- BD.sql
+++ BD.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 30-Out-2013 às 15:16
+-- Data de Criação: 30-Out-2013 às 15:20
 -- Versão do servidor: 5.6.12-log
 -- versão do PHP: 5.4.12
 
@@ -19,6 +19,50 @@ SET time_zone = "+00:00";
 --
 -- Base de Dados: `alocoimbra`
 --
+CREATE DATABASE IF NOT EXISTS `alocoimbra` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `alocoimbra`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluguer`
+--
+
+CREATE TABLE IF NOT EXISTS `aluguer` (
+  `Alojamento` varchar(32) NOT NULL,
+  `Qualidade` set('excelente','muito bom','bom','razoavel') NOT NULL,
+  `Preco_mes` int(11) NOT NULL,
+  `Preco_semana` int(11) NOT NULL,
+  `Preco_dia` int(11) NOT NULL,
+  `Ordem` int(11) NOT NULL,
+  KEY `Ordem` (`Ordem`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `imagens`
+--
+
+CREATE TABLE IF NOT EXISTS `imagens` (
+  `ID` int(11) NOT NULL,
+  `Ordem` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `Ordem` (`Ordem`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `locais`
+--
+
+CREATE TABLE IF NOT EXISTS `locais` (
+  `Nome` varchar(64) NOT NULL,
+  `Descricao` varchar(256) NOT NULL,
+  `Longitude` double NOT NULL,
+  `Latitude` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -61,6 +105,34 @@ CREATE TABLE IF NOT EXISTS `ordem` (
 INSERT INTO `ordem` (`ID`, `Visivel`, `Certificacao`, `Senhorio`, `Lavagem_roupa`, `Mobilado`, `Animais`, `Visitas`, `Morada`, `Zona`, `Proprietario`, `Contacto`, `Email`, `Inscricao_ano`, `Numero_pessoas`, `Min_meses`, `Sexo`, `Cozinha`, `Casa_de_banho`, `Dimensoes`, `Estado_casa`, `Tipo`, `Visita`, `Pagamento`) VALUES
 (0, 1, 1, 0, 0, 0, 0, 1, 'Rua do Caralho, 123', 'A1', 'Filho de uma Pega', 947852468, 'porra_para_isto@fuckit.com', 2013, 1, 12, 'masculino', 'bom', 'muito bom', 'muito bom', 'muito bom', 'quarto', '2013-07-10', '2013-08-06'),
 (1, 1, 1, 1, 1, 1, 1, 1, 'Rua da merda', '69', 'Pila man', 999999999, 'merda@gah.com', 2011, 5, 6, 'feminino', 'razoavel', 'razoavel', 'razoavel', 'razoavel', 'apartamento', '2013-10-01', '2013-10-01');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `zonas`
+--
+
+CREATE TABLE IF NOT EXISTS `zonas` (
+  `Nome` varchar(4) NOT NULL,
+  `Longitude` double NOT NULL,
+  `Latitude` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `aluguer`
+--
+ALTER TABLE `aluguer`
+  ADD CONSTRAINT `aluguer_ibfk_1` FOREIGN KEY (`Ordem`) REFERENCES `ordem` (`ID`);
+
+--
+-- Limitadores para a tabela `imagens`
+--
+ALTER TABLE `imagens`
+  ADD CONSTRAINT `imagens_ibfk_1` FOREIGN KEY (`Ordem`) REFERENCES `ordem` (`ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
