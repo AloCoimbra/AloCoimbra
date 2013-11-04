8cc92ad6be7febe6b774f3218792978bcfa9d382
Lamouse <paulo_pereira_6@hotmail.com>
2013-11-04 20:32:27 +0000

    a parte do Christophe

---
+++ site/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>AloCoimbra</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+<div id="corpo">
+	<div id = "logo">
+    	<a href="#"><img src="images/Topo.png"; alt="AloCoimbra"; title="AloCoimbra"></a>
+    </div>
+	<div id = "menu">              
+    	<ul>
+        	<li><a href="#">Lista</a></li>
+            <li><a href="#">Mapa</a></li>
+            <li><a href="#">Contactos</a></li>
+        </ul>  	
+    </div>
+
+    
+    <div id = "conteudo"> 
+    	<div id="left">
+        	<div id="post">
+        		<titulo>Pesquisa:</titulo>
+            </div>
+            <pesquisa>
+            Tipo:
+                	
+          </pesquisa>
+        </div>
+                
+        <div id="right">a
+        </div>
+    </div> 
+</div>
+        
+</body>
+</html>
