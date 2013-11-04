6c27a4320c43d13597776914b52629b73c309f51
Lamouse <paulo_pereira_6@hotmail.com>
2013-11-04 21:03:41 +0000

    adicionei o botão like, mas tem um bug

--- site/index.php
+++ site/index.php
@@ -1,41 +1,54 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>AloCoimbra</title>
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-
-</head>
-
-<body>
-<div id="corpo">
-	<div id = "logo">
-    	<a href="#"><img src="images/Topo.png"; alt="AloCoimbra"; title="AloCoimbra"></a>
-    </div>
-	<div id = "menu">              
-    	<ul>
-        	<li><a href="#">Lista</a></li>
-            <li><a href="#">Mapa</a></li>
-            <li><a href="#">Contactos</a></li>
-        </ul>  	
-    </div>
-
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>AloCoimbra</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
     
-    <div id = "conteudo"> 
-    	<div id="left">
-        	<div id="post">
-        		<titulo>Pesquisa:</titulo>
+    </head>
+
+    <body>
+    	<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+    
+        <div id="corpo">
+            <div id = "logo">
+                <a href="#"><img src="images/Topo.png"; alt="AloCoimbra"; title="AloCoimbra"></a>
             </div>
-            <pesquisa>
-            Tipo:
-                	
-          </pesquisa>
+            <div id = "menu">              
+                <ul>
+                    <li><a href="#">Lista</a></li>
+                    <li><a href="#">Mapa</a></li>
+                    <li><a href="#">Contactos</a></li>
+                    
+                    <div align="right">
+                    	<div class="fb-like" data-href="https://www.facebook.com/AloCoimbra" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="false" data-send="false"></div>
+                    </div>
+                </ul>  	
+            </div>
+        
+            
+            <div id = "conteudo"> 
+                <div id="left">
+                    <div id="post">
+                        <titulo>Pesquisa:</titulo>
+                    </div>
+                    <pesquisa>
+                    Tipo:
+                            
+                  </pesquisa>
+                </div>
+                        
+                <div id="right">a
+                </div>
+            </div> 
         </div>
                 
-        <div id="right">a
-        </div>
-    </div> 
-</div>
-        
-</body>
+    </body>
 </html>
