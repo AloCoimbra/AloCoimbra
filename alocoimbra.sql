650da728bd2680cb868857b511c3f7996fd266c8
metalhead6666 <noe.godinho.92@gmail.com>
2013-11-23 18:44:20 +0000

    Adicionados quartos até à linha 27 do xls. Adicionado campo "avaliador" em "ordem".
    Também foram actualizados outros campos

--- alocoimbra.sql
+++ alocoimbra.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.8.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Máquina: 127.0.0.1
--- Data de Criação: 22-Nov-2013 às 01:40
--- Versão do servidor: 5.6.11-log
--- versão do PHP: 5.4.14
+-- Máquina: localhost
+-- Data de Criação: 23-Nov-2013 às 18:41
+-- Versão do servidor: 5.6.12-log
+-- versão do PHP: 5.4.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de Dados: `alocoimbra`
 --
+CREATE DATABASE IF NOT EXISTS `alocoimbra` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `alocoimbra`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `aluguers` (
   `ID` int(11) NOT NULL,
-  `Alojamento` varchar(32) NOT NULL,
+  `Alojamento` set('Individual','Duplo') NOT NULL,
   `Qualidade` set('excelente','muito bom','bom','razoavel') NOT NULL,
   `Preco_mes` int(11) NOT NULL,
   `Ordem` int(11) NOT NULL,
@@ -40,9 +42,30 @@ CREATE TABLE IF NOT EXISTS `aluguers` (
 --
 
 INSERT INTO `aluguers` (`ID`, `Alojamento`, `Qualidade`, `Preco_mes`, `Ordem`) VALUES
-(1, 'Individual', 'muito bom', 140, 1),
-(2, 'Duplo', 'muito bom', 100, 2),
-(3, 'Individual', 'bom', 155, 3);
+(1, 'Individual', 'muito bom', 210, 1),
+(2, 'Individual', 'muito bom', 180, 1),
+(3, 'Individual', 'muito bom', 140, 2),
+(4, 'Individual', 'razoavel', 170, 3),
+(5, 'Individual', 'bom', 150, 4),
+(6, 'Individual', 'razoavel', 220, 5),
+(7, 'Individual', 'bom', 220, 6),
+(8, 'Individual', 'muito bom', 155, 7),
+(9, 'Individual', 'muito bom', 160, 7),
+(10, 'Individual', 'muito bom', 200, 8),
+(11, 'Individual', 'muito bom', 150, 9),
+(12, 'Individual', 'bom', 250, 10),
+(13, 'Individual', 'razoavel', 210, 11),
+(14, 'Individual', 'muito bom', 200, 12),
+(15, 'Individual', 'muito bom', 220, 12),
+(16, 'Duplo', 'razoavel', 100, 13),
+(17, 'Individual', 'bom', 200, 14),
+(18, 'Individual', 'razoavel', 130, 15),
+(19, 'Individual', 'razoavel', 140, 15),
+(20, 'Individual', 'muito bom', 160, 16),
+(21, 'Individual', 'muito bom', 160, 17),
+(22, 'Individual', 'bom', 190, 18),
+(23, 'Individual', 'razoavel', 140, 19),
+(24, 'Individual', 'bom', 180, 20);
 
 -- --------------------------------------------------------
 
@@ -109,10 +132,11 @@ CREATE TABLE IF NOT EXISTS `ordens` (
   `Numero_pessoas` int(11) NOT NULL,
   `Min_meses` int(11) NOT NULL,
   `Sexo` set('masculino','feminino','ambos') NOT NULL,
-  `Cozinha` set('excelente','muito bom','bom','razoavel') NOT NULL,
-  `Casa_de_banho` set('excelente','muito bom','bom','razoavel') NOT NULL,
-  `Dimensoes` set('excelente','muito bom','bom','razoavel') NOT NULL,
-  `Estado_casa` set('excelente','muito bom','bom','razoavel') NOT NULL,
+  `Cozinha` set('excelente','muito bom','bom','razoavel','') NOT NULL,
+  `Casa_de_banho` set('excelente','muito bom','bom','razoavel','') NOT NULL,
+  `Dimensoes` set('excelente','muito bom','bom','razoavel','') NOT NULL,
+  `Estado_casa` set('excelente','muito bom','bom','razoavel','') NOT NULL,
+  `Avaliador` varchar(64) NOT NULL,
   `Tipo` set('apartamento','quarto','residencia','republica') NOT NULL,
   `Visita` date NOT NULL,
   `Pagamento` date NOT NULL,
@@ -123,10 +147,27 @@ CREATE TABLE IF NOT EXISTS `ordens` (
 -- Extraindo dados da tabela `ordens`
 --
 
-INSERT INTO `ordens` (`ID`, `Visivel`, `Certificacao`, `Senhorio`, `Lavagem_roupa`, `Mobilado`, `Animais`, `Visitas`, `Agua`, `Luz`, `Gas`, `Internet`, `Outras_despesas`, `Morada`, `Zona`, `Proprietario`, `Contacto`, `Email`, `Inscricao_ano`, `Numero_pessoas`, `Min_meses`, `Sexo`, `Cozinha`, `Casa_de_banho`, `Dimensoes`, `Estado_casa`, `Tipo`, `Visita`, `Pagamento`) VALUES
-(1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, '', 'Rua António José de Almeida, 65 - R/c', 'D3', 'Adilia dos Santos Sousa', '239828611;963535340', '', 2011, 4, 12, 'feminino', 'muito bom', 'muito bom', 'muito bom', 'muito bom', 'quarto', '2013-11-01', '2013-11-01'),
-(2, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 'TV cabo', 'Rua da saragoça, nº2', 'D3', 'Ana Luisa Martins Leite', '966124420', '', 2010, 2, 12, 'ambos', 'muito bom', 'muito bom', 'muito bom', 'muito bom', 'quarto', '2013-11-02', '2013-11-02'),
-(3, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, '', 'Rua Padre António Vieira, 16, nº1', 'D4', 'António Manuel Fonseca Oliveira Neto', '967766049', '', 2012, 10, 6, 'ambos', 'bom', 'muito bom', 'bom', 'muito bom', 'quarto', '2013-11-14', '2013-11-14');
+INSERT INTO `ordens` (`ID`, `Visivel`, `Certificacao`, `Senhorio`, `Lavagem_roupa`, `Mobilado`, `Animais`, `Visitas`, `Agua`, `Luz`, `Gas`, `Internet`, `Outras_despesas`, `Morada`, `Zona`, `Proprietario`, `Contacto`, `Email`, `Inscricao_ano`, `Numero_pessoas`, `Min_meses`, `Sexo`, `Cozinha`, `Casa_de_banho`, `Dimensoes`, `Estado_casa`, `Avaliador`, `Tipo`, `Visita`, `Pagamento`) VALUES
+(1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 'Limpeza', 'Rua do Loureiro, 17', 'D4', 'Fátima Esteves', '239 829 438;964 396 582', '', 2000, 16, 12, 'masculino', '', '', '', '', '', 'quarto', '2013-08-29', '2013-08-29'),
+(2, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, '', 'Rua António José de Almeida, 65 - R/C ', 'D3', 'Adília dos Santos Sousa', '239 828 611;963 535 340', '', 2000, 4, 12, 'feminino', '', '', '', '', '', 'quarto', '2013-08-03', '2013-08-30'),
+(3, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 'Limpeza', 'Avenida Sá da Bandeira, 110 - 1º', 'D4', 'Maria Luísa Baptista Mendes Gouveia', '239 717 303; 91 484 50 50', '', 2000, 4, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-09-07', '2013-09-17'),
+(4, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 'Limpeza', 'Rua do Brasil, 302 - 6º dtº (Calhabé)', 'E6', 'Maria da Glória Bernardo', '239 492 085; 911 990 548', '', 2000, 3, 12, 'masculino', '', '', '', '', '', 'quarto', '2013-08-22', '2013-08-22'),
+(5, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 'Limpeza', 'Rua Venâncio Rodrigues, nº 11 F (Praça da República) ', 'D4', 'Maria José Varandas Martins Silva', '968 203 553; 968 257 816; 917 508 813', 'mjmrst@gmail.com', 2000, 7, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-09-03', '2013-08-03'),
+(6, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 'Limpeza', 'Rua Dr. António de Vasconcelos, 119 - 2º', 'D3', 'Esmeralda Maria Machado Santos Vaz', '239 828 563; 966 052 670', '', 2000, 3, 12, 'masculino', '', '', '', '', '', 'quarto', '2013-03-22', '2013-08-22'),
+(7, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, '', 'Rua Dr. Augusto Rocha, 12', 'E3', 'Aida Fernanda Rodrigues', '91 915 37 44', '', 2000, 7, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-09-03', '2013-09-03'),
+(8, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 'Limpeza Semanal', 'Rua Pinheiro Chagas, 17 - 1º', 'F3', 'Ana Maria Pessoa Monteiro do Vale', '93 361 80 01; 239 483 618', 'anapessoavale@hotmail.com', 2000, 5, 12, 'feminino', '', '', '', '', '', 'quarto', '2013-08-21', '2013-08-21'),
+(9, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 'Limpeza', 'Rua Luís de Camões, 58 - 1º', 'F3', 'Maria do Rosário Duarte Dias', '965 007 818', 'mrosariodd@sapo.pt', 2000, 3, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-08-03', '2013-08-30'),
+(10, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, '', 'Rua Adelino Veiga, 23', 'C4', 'António João Rodrigues Freitas', '919 676 121', '', 2000, 10, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-07-03', '2013-08-01'),
+(11, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 'Limpeza, TV Cabo', 'Rua da Matemática, 10', 'D4', 'Albano Gomes Ventura', '239 824 351; 911 721 476', '', 2000, 14, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-09-11', '2013-09-11'),
+(12, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 'Limpeza', 'Rua da Manutenção Militar, 6', 'D3', 'Lucília de Jesus José Gil', '919 350 335', '', 2000, 11, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-09-17', '2013-09-18'),
+(13, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 'TV Cabo', 'Rua da Matemática, 2 - 2º', 'D4', 'Maria Lisete Soares', '239 820 929; 915 386 933', '', 2000, 10, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-06-25', '2013-06-25'),
+(14, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 'Limpeza', 'Rua Padre António Vieira, 4', 'D4', 'Cassilda de Jesus Fernandes', '239 824 748; 919 879 173', '', 2000, 10, 12, 'feminino', '', '', '', '', '', 'quarto', '2013-06-18', '2013-06-18'),
+(15, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 'Limpeza', 'Avenida Sá da Bandeira, 110 - 1º', 'D4', 'Maria Luísa Baptista Mendes Gouveia', '239 717 303; 91 484 50 50', '', 2000, 4, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-09-17', '2013-09-17'),
+(16, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 'Limpeza', 'Rua Adolfo Loureiro, 57 - 3º Esqª (Solum)', 'F6', 'Maria Rita Dias de Almeida Leite', '963 016 212; 919 520 981', 'm.rita.a.leite@gmail.com', 2000, 4, 12, 'feminino', '', '', '', '', '', 'quarto', '2013-08-03', '2013-08-30'),
+(17, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 'Limpeza', 'Rua Adolfo Loureiro, 118 - Cave Dtª', 'F6', 'Célia Maria Cravo Silvestre Batista', '919 868 735; 969 776 220', 'celia.batista@sapo.pt', 2000, 3, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-06-25', '2013-06-25'),
+(18, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 'Limpeza', 'Rua Guerra Junqueiro, 104 - 2º', 'D3', 'Maria Cândida São José Soares', '914 268 457; 239 832 684; 967 819 632', '', 2000, 5, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-08-09', '2013-09-02'),
+(19, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 'Limpeza', 'Rua Bernardo de Albuquerque, 104', 'F2', 'Maria Jerónima Soares de Albergaria Cabral', '239 482 212', 'coimbra104@gmail.com', 2000, 1, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-09-13', '2013-09-13'),
+(20, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, '', 'Rua António Jardim, 101 - 1º', 'G2', 'Ruben Manuel Antunes Tavares', '239 838 023; 919 975 114', 'ruben@avscentro.min-saude.pt', 2000, 3, 12, 'ambos', '', '', '', '', '', 'quarto', '2013-07-24', '2013-07-24');
 
 -- --------------------------------------------------------
 
