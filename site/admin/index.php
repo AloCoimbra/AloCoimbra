d59224fcd9b3a2aad1fd48b0ad30284d05eac7e5
Jaliborc <contact@jaliborc.com>
2013-12-16 22:22:09 +0000

    Preventing index errors

--- site/admin/index.php
+++ site/admin/index.php
@@ -1,8 +1,8 @@
 <?
 	session_start();
 
-	if ($_POST['password'] == '123')
+	if (@($_POST['password']) == '123')
 		$_SESSION['logged'] = true;
 
-	include $_SESSION['logged'] ? 'list.php': 'login.html';
+	include @($_SESSION['logged']) ? 'list.php': 'login.html';
 ?>
