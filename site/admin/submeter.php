5125aed62d4fb3315183e1293d192da8d9e3d920
Jaliborc <contact@jaliborc.com>
2013-12-17 11:16:53 +0000

    Now edit cheeks for credentials

--- site/admin/submeter.php
+++ site/admin/submeter.php
@@ -1,26 +1,30 @@
 <?
-	require '../database.php';
+    session_start();
 
-	$id = @((int) $_POST['ID']);
-	$args = array();
-	$aluguers->Delete("WHERE Ordem=$id");
+    if ($_SESSION['logged']) {
+		require '../database.php';
 
-	foreach ($ordens->Columns() as $key)
-		$args[$key] = @mysql_real_escape_string($_POST[$key]);
-
-	if ($id >= 0)
-		$ordens->Update($args, "WHERE ID=$id");
-	else
-		$id = $ordens->Insert($args);
-
-	for ($i=0; @($_POST['Alojamento' . $i]); $i++) { 
+		$id = @((int) $_POST['ID']);
 		$args = array();
-		$args['Ordem'] = $id;
+		$aluguers->Delete("WHERE Ordem=$id");
 
-		foreach ($aluguers->Columns() as $key)
-			$args[$key] = @mysql_real_escape_string($_POST[$key . $i]);
+		foreach ($ordens->Columns() as $key)
+			$args[$key] = @mysql_real_escape_string($_POST[$key]);
 
-		$aluguers->Insert($args);
+		if ($id >= 0)
+			$ordens->Update($args, "WHERE ID=$id");
+		else
+			$id = $ordens->Insert($args);
+
+		for ($i=0; @($_POST['Alojamento' . $i]); $i++) { 
+			$args = array();
+			$args['Ordem'] = $id;
+
+			foreach ($aluguers->Columns() as $key)
+				$args[$key] = @mysql_real_escape_string($_POST[$key . $i]);
+
+			$aluguers->Insert($args);
+		}
 	}
 
 	header('Location: /admin/');
